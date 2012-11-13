ec8c127ea5af0a04207dcaddc1eb656a9072266b
Kristoffer Ahl <git@77dynamite.com>
2012-11-13 23:52:09 +0100

    - Removed build-local and build-release files from Source\Koshu. - Added build-trigger.cmd to Templates directory.

---
+++ Source/Koshu/Templates/build-trigger.cmd
@@ -0,0 +1,2 @@
+CALL koshu.cmd build buildFile.ps1 Target
+PAUSE
--- Source/Koshu/build-release.cmd
+++
@@ -1,2 +0,0 @@
-powershell.exe -NoProfile -NonInteractive -InputFormat none -ExecutionPolicy unrestricted "koshu-trigger.ps1" -BuildFile ".\build.ps1" -Target Release
-PAUSE
