8da4f1841b0075ffbf67726cfb5a537504474736
Kristoffer Ahl <git@77dynamite.com>
2012-11-13 23:54:46 +0100

    - Disabled warnings for importing koshu module in install.ps1. - Calling Koshu-Scaffold from intall.ps1 with default parameters.

--- Source/Koshu/install.ps1
+++ Source/Koshu/install.ps1
@@ -1,14 +1,7 @@
 param($installPath, $toolsPath, $package, $project)
 
+$rootDir = (resolve-path "$installPath\..\..\..\")
 $koshuModule = Join-Path $toolsPath koshu.psm1
-import-module $koshuModule
 
-$rootDir			= (resolve-path "$installPath\..\..\..\")
-$toolsDirRel		= $toolsPath -replace [regex]::Escape($rootDir), ".\"
-
-if (!(test-path "$rootDir\build.ps1")) {
-	copy_files "$toolsPath\Templates" $rootDir "build.ps1"
-}
-
-(cat "$toolsPath\build-local.cmd") -replace "koshu-trigger.ps1","$toolsDirRel\koshu-trigger.ps1" | out-file "$rootDir\build-local.cmd" -encoding "Default" -force
-(cat "$toolsPath\build-release.cmd") -replace "koshu-trigger.ps1","$toolsDirRel\koshu-trigger.ps1" | out-file "$rootDir\build-release.cmd" -encoding "Default" -force
+Import-Module $koshuModule -DisableNameChecking
+Koshu-Scaffold build $project.Name $rootDir
