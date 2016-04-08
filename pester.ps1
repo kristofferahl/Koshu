24cf5f1448873aa5bbd98c64fe5c19e3c73b52c9
Kristoffer Ahl <git@77dynamite.com>
2016-04-08 17:02:47 +0200

    - Updated Pester to the latest verion 3.4.0.

--- pester.ps1
+++ pester.ps1
@@ -1,12 +1,11 @@
+$pesterVersion = '3.4.0'
 $nuget = (resolve-path '.\.nuget\nuget.exe')
 
-& $nuget install Pester -Version 2.0.3 -OutputDirectory "Source\packages"
+& $nuget install Pester -Version $pesterVersion -OutputDirectory "Source\packages"
 
 rmdir 'Build\Temp' -recurse -force -erroraction silentlycontinue | out-null
 mkdir 'Build\Temp' -force | out-null
 & $nuget pack 'Source\Koshu.nuspec' -Version 0.0.1 -OutputDirectory "Build\Temp" -NoPackageAnalysis
 
-$pesterDir = '.\Source\packages\Pester.2.0.3\tools'
-Import-Module "$pesterDir\Pester.psm1"
-
+Import-Module ".\Source\packages\Pester.$pesterVersion\tools\Pester.psm1"
 Invoke-Pester '.\Source\Specifications\*'
