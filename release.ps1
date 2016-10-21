6cea8ec8dda8caaaefd44f7d87b27deeb187cd6c
Kristoffer Ahl <git@77dynamite.com>
2016-10-21 13:02:30 +0200

    - Enabled building multiple nuget packages.

--- release.ps1
+++ release.ps1
@@ -1,10 +1,11 @@
 $version = get-content ".\.version"
 $buildNumber = "$($env:APPVEYOR_BUILD_NUMBER)"
-if ($buildNumber -ne $null -and $buildNumber -ne '') {
-	$version = "$version.$buildNumber"
-}
 
 write-host "Packaging Koshu v.$version" -fore yellow
 
 mkdir "Build\Artifacts" -erroraction silentlycontinue | out-null
 nuget pack "Source\Koshu.nuspec" -outputdirectory "Build\Artifacts" -version $version
+
+if ($buildNumber -ne $null -and $buildNumber -ne '') {
+	nuget pack "Source\Koshu.nuspec" -outputdirectory "Build\Artifacts" -version "$version.$buildNumber"
+}
