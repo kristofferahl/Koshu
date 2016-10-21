046f22ddabd007c53fe5717aa265b61f33f8b05f
Kristoffer Ahl <git@77dynamite.com>
2016-10-21 10:32:50 +0200

    - Fixing release versioning.

--- release.ps1
+++ release.ps1
@@ -1,10 +1,10 @@
 $version = get-content ".\.version"
-$buildNumber = $env:APPVEYOR_BUILD_NUMBER
+$buildNumber = "$($env:APPVEYOR_BUILD_NUMBER)"
 if ($buildNumber -ne $null -and $buildNumber -ne '') {
-	$verion = "$version.$buildNumber"
+	$version = "$version.$buildNumber"
 }
 
-write-host "Current version: $version" -fore yellow
+write-host "Packaging Koshu v.$version" -fore yellow
 
 mkdir "Build\Artifacts" -erroraction silentlycontinue | out-null
-nuget pack "Source\Koshu.nuspec" -outputdirectory "Build\Artifacts"
+nuget pack "Source\Koshu.nuspec" -outputdirectory "Build\Artifacts" -version $version
