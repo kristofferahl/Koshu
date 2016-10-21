43a2ba6f4601de39945d90c73d72ebcb6d42fe08
Kristoffer Ahl <git@77dynamite.com>
2016-10-21 16:19:32 +0200

    - Ensured we use nuget.exe from .nuget directory when creating release.

--- release.ps1
+++ release.ps1
@@ -3,7 +3,8 @@ $buildNumber = "$($env:APPVEYOR_BUILD_NUMBER)"
 if ($buildNumber -ne $null -and $buildNumber -ne '') {
 	$version = "$version.$buildNumber"
 }
+$nuget = (resolve-path '.\.nuget\nuget.exe')
 
 write-host "Packaging Koshu v.$version" -fore yellow
 mkdir "Build\Artifacts" -erroraction silentlycontinue | out-null
-nuget pack "Source\Koshu.nuspec" -outputdirectory "Build\Artifacts" -version $version
+& $nuget pack "Source\Koshu.nuspec" -outputdirectory "Build\Artifacts" -version $version
